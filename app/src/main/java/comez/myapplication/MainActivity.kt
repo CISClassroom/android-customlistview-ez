5ac28b73c3f02e2eeec42ab3d8e59c85d47a0458
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2020-01-03 11:10:07 +0700

    Add ProfileActivity

--- app/src/main/java/comez/myapplication/MainActivity.kt
+++ app/src/main/java/comez/myapplication/MainActivity.kt
@@ -2,8 +2,10 @@ package comez.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,11 +16,19 @@ class MainActivity : AppCompatActivity() {
         val name = arrayOf(
             "OOP", "JAVA", "KOTLIN"
         )
+        val student_name = resources.getStringArray(R.array.student_name)
+
         // data -> Adapter -> View. listview
         val mylistview = findViewById<ListView>(R.id.mylistview)
         val arrayAdapter: ArrayAdapter<*>
         arrayAdapter = ArrayAdapter(this,
-            android.R.layout.simple_list_item_1, name)
+            android.R.layout.simple_list_item_1, student_name)
         mylistview.adapter = arrayAdapter // as ListAdapter
+
+        mylistview.setOnItemClickListener{parent, view, position, id ->
+            Log.i("hi",position.toString())
+            val itemText = parent.getItemAtPosition(position) as String
+            Toast.makeText(this,itemText.toString(),Toast.LENGTH_SHORT).show()
+        }
     }
 }
