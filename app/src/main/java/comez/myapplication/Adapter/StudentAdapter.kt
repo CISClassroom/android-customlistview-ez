5c61f078b7948d05b0704904f8a818254da2b6c7
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2020-01-06 11:34:19 +0700

    update listview.adapter

--- app/src/main/java/comez/myapplication/Adapter/StudentAdapter.kt
+++ app/src/main/java/comez/myapplication/Adapter/StudentAdapter.kt
@@ -1,17 +1,35 @@
 package comez.myapplication.Adapter
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
 import comez.myapplication.Model.Student
+import comez.myapplication.R
+import kotlinx.android.synthetic.main.listitem_student.view.*
 
 public class StudentAdapter(val mCtx: Context,
                             var resource:Int,
                             var items:List<Student>)
     : ArrayAdapter<Student> (mCtx,resource,items){
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        return super.getView(position, convertView, parent)
+        //return super.getView(position, convertView, parent)
+        val layout:LayoutInflater = LayoutInflater.from(mCtx)
+
+        val v:View = layout.inflate(resource,null)
+        val icon : ImageView = v.findViewById(R.id.imageView)
+        val name : TextView = v.findViewById(R.id.textView)
+        val id:TextView = v.findViewById(R.id.textView2)
+
+        val student:Student = items[position]
+
+        name.text = student.name
+        id.text = student.id
+        icon.setImageDrawable(mCtx.getDrawable(student.icon))
+        return v
     }
 }
 
--- app/src/main/java/comez/myapplication/CustomListActivity.kt
+++ app/src/main/java/comez/myapplication/CustomListActivity.kt
@@ -2,11 +2,26 @@ package comez.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ListView
+import comez.myapplication.Adapter.StudentAdapter
+import comez.myapplication.Model.Student
 
 class CustomListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_custom_list)
+
+        var student:Student = Student("Wannaphong","603410214-3",R.mipmap.ic_launcher)
+
+        var students = mutableListOf<Student>()
+
+        students.add(student)
+        students.add(Student("M","02",R.mipmap.ic_launcher))
+
+        var listview:ListView = findViewById(R.id.customlistview)
+
+        listview.adapter = StudentAdapter(this,
+            R.layout.listitem_student,students)
     }
 }
