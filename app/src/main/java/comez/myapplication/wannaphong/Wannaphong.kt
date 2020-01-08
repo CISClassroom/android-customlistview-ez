1a46bd5cb2ab481c7ce57ba85edf4549eb52b476
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2020-01-08 14:27:33 +0700

    Add activity_wannaphong

---
+++ app/src/main/java/comez/myapplication/wannaphong/Wannaphong.kt
@@ -0,0 +1,13 @@
+package comez.myapplication.wannaphong
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import comez.myapplication.R
+
+class Wannaphong : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_wannaphong)
+    }
+}
